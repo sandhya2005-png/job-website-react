15ad5bbf660f19c65efa222db7584040c8517f67
sandhya2005-png <146368121+sandhya2005-png@users.noreply.github.com>
2025-11-18 08:55:26 +0530

    Final Changes

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import {Outlet} from 'react-router-dom';
 import Navbar from '../components/Navbar';
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify';
 
 const MainLayout = () => {
   return (
     <>
       <Navbar />
       <Outlet />
+      <ToastContainer />
     </>
   )
 }
